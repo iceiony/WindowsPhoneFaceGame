ae4f74e7ef0cf48140447049c236da4b3df79313
iceiony <iceiony@yahoo.com>
2014-07-05 23:32:59 +0100

    change colour of score depending if positive or negative

---
+++ src/FaceGame/Converters/ScoreToColour.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace FaceGame.Converters
+{
+    public class ScoreToColour:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var score = (int)value;
+            if (score >= 0)
+                return "#1DD300";
+            else
+                return "#FF5300";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
